abfd1a69391b19d6a4997dcb1e305bad83aea691
JuanCervino <juan.cer.remer4@gmail.com>
2025-04-14 09:58:05 -0400

    running experiments cartpole

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,72 +1,22 @@
-# For Two Moons Dataset
-# 2 Connected Components
-# python -m smooth.scripts.toy_example_manifold  --algorithm ERM\
-#                        --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0. --lr 0.9\
-#                         --regularizer 0. --heat_kernel_t 0.005\
-#                         --normalize True --noise 0.05
+#!/bin/bash
+#SBATCH --job-name imitation_smoothness
+#SBATCH -o energy_imitation_%j.log
+#SBATCH --gres=gpu:volta:1
 
-# python -m smooth.scripts.toy_example_manifold  --algorithm ERM\
-#                        --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0.1 --lr 0.9\
-#                         --regularizer 0. --heat_kernel_t 0.005\
-#                         --normalize True --noise 0.05
+# Initialize the module command first source
+source /etc/profile
 
-# python -m smooth.scripts.toy_example_manifold  --algorithm LAPLACIAN_REGULARIZATION\
-#                        --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0. --lr 0.9\
-#                         --regularizer 0.5 --heat_kernel_t 0.005\
-#                         --normalize True --noise 0.05
+# Then use the module command to load the module needed for your work
+module load anaconda/Python-ML-2024b
+# module load anaconda/Python-ML-2023b # clu issue
 
-# python -m smooth.scripts.toy_example_manifold  --algorithm MANIFOLD_GRADIENT_NO_RHO\
-#                        --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0. --lr 2\
-#                         --regularizer 1 --heat_kernel_t 0.005\
-#                         --normalize True --noise 0.05 --epsilon 0.02
+nvcc --version
+nvidia-smi
 
+# python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 100 --n_test 1000 --bs 1000 --lr 0.1 --algorithm ERM
+# python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 100 --n_test 1000 --bs 1000 --lr 0.1 --heat_kernel_t 0.09 --clamp 0.9  --algorithm LaplacianRegularizationEuclidean --regularizer 0.001
+# python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 100 --n_test 1000 --bs 1000 --lr 0.1 --heat_kernel_t 0.1 --clamp 0.09  --algorithm LaplacianRegularizationMomentum --regularizer 0.00001
 
-# # 1 Connected Components
-# python -m smooth.scripts.toy_example_manifold  --algorithm MANIFOLD_GRADIENT_NO_RHO\
-#                         --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0. --lr .9\
-#                         --regularizer 1 --heat_kernel_t 0.005\
-#                         --normalize True --noise 0.1 --epsilon 0.2
-
-
-# python -m smooth.scripts.toy_example_manifold  --algorithm MANIFOLD_GRADIENT_NO_RHO\
-#                         --n_train 1 --n_unlab 200 --epochs 15000\
-#                         --hidden_neurons 64 --weight_decay 0. --lr .9\
-#                         --regularizer 1 --heat_kernel_t 0.019\
-#                         --normalize True --noise 0.1 --epsilon 0.2
-
-
-
-
-
-python -m smooth.scripts.quadrotor_state_prediction --algorithm ERM\
-                        --epochs 10000 --bs 5999\
-                        --hidden_neurons 8192 --weight_decay 0.2 --lr 0.00001\
-                        --regularizer 0.000001 --heat_kernel_t 0.004\
-                        --normalize True --epsilon 0.1 --weight_decay 0. --heat_kernel_t 0.01
-
-
-# python -m smooth.scripts.quadrotor_state_prediction --algorithm ERM\
-#                         --epochs 10000 --bs 5999\
-#                         --hidden_neurons 8192 --weight_decay 0.2 --lr 0.00001\
-#                         --regularizer 0.000001 --heat_kernel_t 0.004\
-#                         --normalize True --epsilon 0.1 --weight_decay 0.1 --heat_kernel_t 0.01
-
-# python -m smooth.scripts.quadrotor_state_prediction --algorithm LAPLACIAN_REGULARIZATION\
-#                         --epochs 10000 --bs 5999\
-#                         --hidden_neurons 8192 --weight_decay 0. --lr 0.00001\
-#                         --regularizer 0.000001\
-#                         --normalize True --epsilon 0.1\
-#                         --weight_decay 0. --heat_kernel_t 0.01
-
-# python -m smooth.scripts.quadrotor_state_prediction --algorithm MANIFOLD_GRADIENT_NO_RHO\
-#                         --epochs 10000 --bs 5999\
-#                         --hidden_neurons 8192 --weight_decay 0. --lr 0.00001\
-#                         --regularizer 0.000001\
-#                         --normalize True --epsilon 0.03\
-#                         --weight_decay 0. --heat_kernel_t 0.01
-
+python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 1000 --n_test 1000 --bs 1000 --lr 0.025 --heat_kernel_t 0.1 --clamp 0.09  --algorithm MomentumGradient --regularizer 0.00001 
+# python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 100 --n_test 1000 --bs 1000 --lr 0.04 --heat_kernel_t 0.1 --clamp 0.09  --algorithm MomentumGradient --regularizer 0.00001 
+# python -m smooth.scripts.imitation_learning --epochs 70000 --n_train 1000 --n_test 1000 --bs 1000 --lr 0.02 --heat_kernel_t 0.09 --clamp 0.9  --algorithm ManifoldGradient --regularizer 0.00001
--- smooth/scripts/imitation_learning.py
+++ smooth/scripts/imitation_learning.py
@@ -113,7 +113,7 @@ def main(args):
                 optimizer.step()
 
 
-            if epoch % 100==0:
+            if (epoch+1) % args.print_steps == 0 :
                 with torch.no_grad():           
                     loss_test = mse_metric(net,test_loader,device)
                     loss_train = mse_metric(net,train_loader,device)
@@ -123,7 +123,8 @@ def main(args):
     elif args.algorithm == 'LaplacianRegularizationEuclidean':
         columns = ['Epoch', 'Loss','Regularized Laplacian Loss', 'Laplacian Loss', 'Accuracy']
         utils.create_csv(args.output_dir, 'losses.csv', columns)
-        L = laplacian.get_laplacian(X_train, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp,distance_type = 'euclidean').to(device)
+        X_total = torch.concatenate((X_train, X_test), axis=0)
+        L = laplacian.get_laplacian(X_total, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp,distance_type = 'euclidean').to(device)
 
         e, V = np.linalg.eig(L.cpu().detach().numpy())
         print('Connected Components', np.sum(e < 0.0001))
@@ -133,14 +134,16 @@ def main(args):
                         inputs, labels = data
                         optimizer.zero_grad()
                         f = net(inputs)
+                        f_unlabel = net(X_total)
+
                         loss = F.mse_loss(f, labels)
                         loss_MSE = loss.item()
-                        loss += args.regularizer * torch.trace(torch.matmul(f.transpose(0,1),torch.matmul(L, f)))
+                        loss += args.regularizer * torch.trace(torch.matmul(f_unlabel.transpose(0,1),torch.matmul(L, f_unlabel)))
                         loss.backward()
                         optimizer.step()
                         # print(loss.item(),loss_MSE.item(),(loss-loss_MSE).item())
                     
-                    if epoch % 100 ==0:
+                    if (epoch+1) % args.print_steps ==0:
                         with torch.no_grad():
                             loss_test = mse_metric(net,test_loader,device)
                             loss_train = mse_metric(net,train_loader,device)
@@ -150,7 +153,10 @@ def main(args):
     elif args.algorithm == 'LaplacianRegularizationMomentum':
         columns = ['Epoch', 'Loss','Regularized Laplacian Loss', 'Laplacian Loss', 'Accuracy']
         utils.create_csv(args.output_dir, 'losses.csv', columns)
-        L = laplacian.get_laplacian(X_train, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp,distance_type = 'momentum_pendulum').to(device)
+        print(args.heat_kernel_t, args.clamp, args.normalize)
+        X_total = torch.concatenate((X_train, X_test), axis=0)
+
+        L = laplacian.get_laplacian(X_total, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp,distance_type = 'momentum_pendulum').to(device)
 
         e, V = np.linalg.eig(L.cpu().detach().numpy())
         print('Connected Components', np.sum(e < 0.0001))
@@ -160,21 +166,131 @@ def main(args):
                         inputs, labels = data
                         optimizer.zero_grad()
                         f = net(inputs)
+                        f_unlabel = net(X_total)
                         loss = F.mse_loss(f, labels)
                         loss_MSE = loss.item()
-                        loss += args.regularizer * torch.trace(torch.matmul(f.transpose(0,1),torch.matmul(L, f)))
+                        loss += args.regularizer * torch.trace(torch.matmul(f_unlabel.transpose(0,1),torch.matmul(L, f_unlabel)))
                         loss.backward()
                         optimizer.step()
                         # print(loss.item(),loss_MSE.item(),(loss-loss_MSE).item())
                     
-                    if epoch % 100 ==0:
+                    if (epoch+1) % args.print_steps == 0 :
                         with torch.no_grad():
                             loss_test = mse_metric(net,test_loader,device)
                             loss_train = mse_metric(net,train_loader,device)
                         utils.save_state(args.output_dir,epoch,loss_train,loss_train-loss_MSE,loss_MSE,loss_test)
                         print(f'Epoch {epoch}, loss {loss.item()}, loss diff {loss.item()-loss_MSE}, loss train {loss_train}, loss test {loss_test}')                        
             
-        
+
+    elif args.algorithm == 'ManifoldGradient':
+
+            columns = ['Epoch', 'Loss CE','Regularized Laplacian Loss', 'Laplacian Loss', 'Accuracy']
+            utils.create_csv(args.output_dir, 'losses.csv', columns)
+            X_total = torch.concatenate((X_train, X_test), axis=0)
+            adj_matrix = laplacian.get_pairwise_distance_matrix(X_total, t=args.heat_kernel_t, distance_type='euclidean').to(device)
+
+            L = laplacian.get_laplacian(X_total, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp).to(device)
+            e, V = np.linalg.eig(L.cpu().detach().numpy())
+            print('Connected Components', np.sum(e < 0.0001))
+            lambda_dual = torch.ones(len(X_total[:,0])) / len(X_total[:,0])  # Initialize dual variables for each sample in the dataset
+            lambda_dual = lambda_dual.to(device).detach().requires_grad_(False)
+            mu_dual = 5*torch.ones(1).to(device).detach().requires_grad_(False)
+            for epoch in range(args.epochs):
+                # print(epoch)
+                ############################################
+                # Primal Update
+                ############################################
+                for i, data in enumerate(train_loader):
+                    inputs, labels = data
+                    optimizer.zero_grad()
+                    f = net(inputs)
+                    f_all = net(X_total)
+
+                    loss = mu_dual * F.mse_loss(f, labels)
+                    loss_MSE = loss.item()
+                    loss += args.regularizer * torch.trace(torch.matmul(f_all.transpose(0,1),torch.matmul(L, f_all)))
+                    loss.backward()
+                    optimizer.step()
+                            # print(loss.item(),loss_MSE.item(),(loss-loss_MSE).item())
+                ############################################
+                # Dual Update
+                ############################################
+                if (epoch+1) % args.dual_update_steps ==0 :
+
+                    with torch.no_grad():
+                        # mu_dual = torch.nn.functional.relu(mu_dual + args.dual_step_mu * (F.cross_entropy(net(X_lab), y_lab) - args.epsilon))
+                        mu_dual = torch.clamp(mu_dual + args.dual_step_mu * (F.mse_loss(net(X_train), Y_train) - args.epsilon),0,5)
+                        f_all = net(X_total)  # Forward pass for all samples in the dataset
+
+                        numerator = torch.abs (f_all  - f_all.transpose(0,1)).to(device)
+                        division = torch.div(numerator, (adj_matrix + torch.eye(f_all.shape[0]).to(device)))
+                        [grads,indices] = torch.max(division, 1)
+                        lambda_dual = F.relu(lambda_dual + args.dual_step_mu*(grads))
+                        lambda_dual = lambda_dual/torch.sum(lambda_dual).item()
+
+                if (epoch+1) % args.print_steps ==0 :
+                    with torch.no_grad():
+                        acc = mse_metric(net,test_loader,device)
+                        loss_train = mse_metric(net,train_loader,device)
+                    utils.save_state(args.output_dir,epoch,loss_train,loss_train-loss_MSE,loss_MSE,acc)
+                
+                    print(f'Epoch loss{epoch,loss.item()}, mu={mu_dual.item()}, {loss.item()-loss_MSE}, loss train {loss_train}, loss test {acc}' )
+       
+
+    elif args.algorithm == 'MomentumGradient':
+
+            columns = ['Epoch', 'Loss CE','Regularized Laplacian Loss', 'Laplacian Loss', 'Accuracy']
+            utils.create_csv(args.output_dir, 'losses.csv', columns)
+            X_total = torch.concatenate((X_train, X_test), axis=0)
+            adj_matrix = laplacian.get_pairwise_distance_matrix(X_total, t=args.heat_kernel_t, distance_type='momentum_pendulum').to(device)
+
+            L = laplacian.get_laplacian(X_total, args.normalize, heat_kernel_t=args.heat_kernel_t, clamp_value = args.clamp, distance_type = 'momentum_pendulum').to(device)
+            e, V = np.linalg.eig(L.cpu().detach().numpy())
+            print('Connected Components', np.sum(e < 0.0001))
+            lambda_dual = torch.ones(len(X_total[:,0])) / len(X_total[:,0])  # Initialize dual variables for each sample in the dataset
+            lambda_dual = lambda_dual.to(device).detach().requires_grad_(False)
+            mu_dual = 5*torch.ones(1).to(device).detach().requires_grad_(False)
+            for epoch in range(args.epochs):
+                # print(epoch)
+                ############################################
+                # Primal Update
+                ############################################
+                for i, data in enumerate(train_loader):
+                    inputs, labels = data
+                    optimizer.zero_grad()
+                    f = net(inputs)
+                    f_all = net(X_total)
+
+                    loss = mu_dual * F.mse_loss(f, labels)
+                    loss_MSE = loss.item()
+                    loss += args.regularizer * torch.trace(torch.matmul(f_all.transpose(0,1),torch.matmul(L, f_all)))
+                    loss.backward()
+                    optimizer.step()
+                            # print(loss.item(),loss_MSE.item(),(loss-loss_MSE).item())
+                ############################################
+                # Dual Update
+                ############################################
+                if (epoch+1) % args.dual_update_steps ==0 :
+
+                    with torch.no_grad():
+                        # mu_dual = torch.nn.functional.relu(mu_dual + args.dual_step_mu * (F.cross_entropy(net(X_lab), y_lab) - args.epsilon))
+                        mu_dual = torch.clamp(mu_dual + args.dual_step_mu * (F.mse_loss(net(X_train), Y_train) - args.epsilon),0,5)
+                        f_all = net(X_total)  # Forward pass for all samples in the dataset
+
+                        numerator = torch.abs (f_all  - f_all.transpose(0,1)).to(device)
+                        division = torch.div(numerator, (adj_matrix + torch.eye(f_all.shape[0]).to(device)))
+                        [grads,indices] = torch.max(division, 1)
+                        lambda_dual = F.relu(lambda_dual + args.dual_step_mu*(grads))
+                        lambda_dual = lambda_dual/torch.sum(lambda_dual).item()
+
+                if (epoch+1) % args.print_steps ==0 :
+                    with torch.no_grad():
+                        acc = mse_metric(net,test_loader,device)
+                        loss_train = mse_metric(net,train_loader,device)
+                    utils.save_state(args.output_dir,epoch,loss_train,loss_train-loss_MSE,loss_MSE,acc)
+                
+                    print(f'Epoch loss{epoch,loss.item()}, mu={mu_dual.item()}, {loss.item()-loss_MSE}, loss train {loss_train}, loss test {acc}' )
+              
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='Manifold Regularization with Synthetic Data')
@@ -187,10 +303,13 @@ if __name__ == '__main__':
     parser.add_argument('--n_test', type=int, default=10)
     parser.add_argument('--data_dir', type=str, default='./smooth/data')
     parser.add_argument('--bs', type=int, default=8)
+    parser.add_argument('--print_steps', type=int, default=500)
 
 
     parser.add_argument('--algorithm', type=str, default='ERM')
     parser.add_argument('--epochs', type=int, default=100)
+    parser.add_argument('--dual_update_steps', type=int, default=100)
+
     parser.add_argument('--regularizer', type=float, default=1e-6)
     parser.add_argument('--heat_kernel_t', type=float, default=1e-2)
     parser.add_argument('--normalize', type=bool, default=True)
@@ -198,7 +317,7 @@ if __name__ == '__main__':
     parser.add_argument('--hidden_neurons', type=int, default=64)
     parser.add_argument('--lr', type=float, default=0.001)
     parser.add_argument('--momentum', type=float, default=0.)
-    parser.add_argument('--weight_decay', type=float, default=0.9)
+    parser.add_argument('--weight_decay', type=float, default=0.)
 
     parser.add_argument('--dual_step_mu', type=float, default=0.5)
     parser.add_argument('--dual_step_lambda', type=float, default=0.1)
